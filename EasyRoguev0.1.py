af675c2b5e916fd052d7fab129b7e191628897ac
F4TG33K <louischevalier21@gmail.com>
2016-09-09 22:10:08 +0000

    Keep cleaning the code + monitor mode

--- EasyRoguev0.1.py
+++ EasyRoguev0.1.py
@@ -5,66 +5,56 @@ Contact: fatgeek@gmail.com
 Description: Little script that helps you creating a fake access point
 Version: 0.1 "Just getting the script working, enhancments are coming ;)"
 '''
+
 #Getting ready
-import os
+from os import system
 from time import sleep as slp
 import ERfonctions as mod
 import subprocess as sp
-import math
 
 #Variables
 waitincrement = 0
-jump = 0
+jump = 2
 choicemade = 0
+Refresh = 0
 
 #Message Display
-ask =    "        [" + mod.colors.YELLOW + "?" + mod.colors.DEFAULT + "] "
-answer = "           [" + mod.colors.LCYAN + ">" + mod.colors.DEFAULT + "] "
-error =  "           [" + mod.colors.LRED + "!" + mod.colors.DEFAULT + "] "
-info =   "        [" + mod.colors.LGREEN + "*" + mod.colors.DEFAULT + "] "
+ask =          "        [" + mod.colors.YELLOW + "?" + mod.colors.DEFAULT + "] "
+info =         "        [" + mod.colors.LGREEN + "*" + mod.colors.DEFAULT + "] "
+answer =    "           [" + mod.colors.LCYAN  + ">" + mod.colors.DEFAULT + "] "
+error =     "           [" + mod.colors.LRED   + "!" + mod.colors.DEFAULT + "] "
+errorsoft = "           [" + mod.colors.YELLOW   + "!" + mod.colors.DEFAULT + "] "
 
 #Config Infos
-MODE = "Not Configured"
+MODE      = "Not Configured"
+MONITOR   = "Not Configured"
 DHCPStart = "Not Configured"
-DHCPStop = "Not Configured"
-DNS = "Not Configured"
-ESSID = "Not Configured"
-CHANNEL = "Not Configured"
-PASS = "Not Configured"
-STATUS = "Inactive"
+DHCPStop  = "Not Configured"
+DNS       = "Not Configured"
+ESSID     = "Not Configured"
+CHANNEL   = "Not Configured"
+PASS      = "Not Configured"
+STATUS    = "Inactive"
 
 def InfosHeader():
-    os.system('clear')
+    system('clear')
     #Printing informations about the config of the AP
-    print(mod.colors.LGREEN + 
-  "\n                ************************************" + mod.colors.DEFAULT)
-    print(mod.colors.LCYAN +
-    "                    EasyRogue v0.1 Configuration\n" + mod.colors.DEFAULT
-    )
-    print(
-    "                    Fake AP Mode : " + mod.colors.LRED + (MODE) + mod.colors.DEFAULT)
-    print(
-    "                    ESSID        : " + mod.colors.LRED + ESSID + mod.colors.DEFAULT)
-    print(
-    "                    Channel      : " + mod.colors.LRED + CHANNEL + mod.colors.DEFAULT)
-    print(
-    "                    Password     : "+ mod.colors.LRED + PASS + mod.colors.DEFAULT)
-    print(
-    "                    DHCP Start   : " + mod.colors.LRED + DHCPStart + mod.colors.DEFAULT)
-    print(
-    "                    DHCP Stop    : " + mod.colors.LRED + DHCPStop + mod.colors.DEFAULT)
-    print(
-    "                    DNS Server   : " + mod.colors.LRED + DNS + mod.colors.DEFAULT)
-    print(
-    "                    Status       : " + mod.colors.LRED + STATUS + mod.colors.DEFAULT)
-    print(mod.colors.LGREEN + 
-  "\n                ***********************************\n" + mod.colors.DEFAULT)
+    print(mod.colors.LGREEN + "\n                ************************************" + mod.colors.DEFAULT)
+    print(mod.colors.LCYAN +"                    EasyRogue v0.1 Configuration\n" + mod.colors.DEFAULT)
+    print("                    Fake AP Mode : " + mod.colors.LRED + MODE + mod.colors.DEFAULT)
+    print("                    Monitor Card : " + mod.colors.LRED + MONITOR + mod.colors.DEFAULT)
+    print("                    ESSID        : " + mod.colors.LRED + ESSID + mod.colors.DEFAULT)
+    print("                    Channel      : " + mod.colors.LRED + CHANNEL + mod.colors.DEFAULT)
+    print("                    Password     : " + mod.colors.LRED + PASS + mod.colors.DEFAULT)
+    print("                    DHCP Start   : " + mod.colors.LRED + DHCPStart + mod.colors.DEFAULT)
+    print("                    DHCP Stop    : " + mod.colors.LRED + DHCPStop + mod.colors.DEFAULT)
+    print("                    DNS Server   : " + mod.colors.LRED + DNS + mod.colors.DEFAULT)
+    print("                    Status       : " + mod.colors.LRED + STATUS + mod.colors.DEFAULT)
+    print(mod.colors.LGREEN + "\n                ***********************************\n" + mod.colors.DEFAULT)
 
 while jump == 0:
+    ################Choose the Ap Mode
     mod.WelcomeHeader()
-
-    ################Menu
-
     if (waitincrement == 0):
         slp(1)
         waitincrement = 1
@@ -74,7 +64,7 @@ while jump == 0:
     print(mod.colors.BLUE + "\n            2" + mod.colors.DEFAULT + ") Honey Pot")
     print(mod.colors.BLUE + "\n            3" + mod.colors.DEFAULT + ") Karma-Attack Access Point")
 
-    print("\n"+ ask +"Which one do you want to use ? \n\n")
+    print("\n"+ ask +"Which one do you want to use ? \n")
     APWay = raw_input(answer)
     try:
         APWay = int(APWay)
@@ -100,62 +90,150 @@ while jump == 0:
     
     elif APWay == 2:
         choicemade = 2  #Set the mode
-        MODE = "Honey Pot"
+        MODE = mod.VariableColoring("G","Honey Pot")
         jump = 2        #Set where to go now
         break
     
     else:
-        print("\n           [" + mod.colors.LRED + "!" + mod.colors.DEFAULT + "] Invalide answer !")
+        print("\n"+error+"] Invalide answer !")
         slp(2)
         continue
-
-   ################Setting up DHCP
 while jump == 2:
+    ################Setting up DHCP
+    exit_the_loop = 0
     InfosHeader()
-    print("\n"+ ask+"Starting DHCP address (e.g. 10.0.0.50) ?")
-    DHCPStart = raw_input(answer)
-    DHCPStop = raw_input(answer)
-    
-#Create the dnsmasq config file
-f = open('dnsmasq.conf', 'w')
-f.write('interface=at0\n' +
-'dhcp-range='+ DHCPStart + ',' + DHCPStop + ',12h\n' +
-'server=80.67.169.12\n' +
-'server=80.67.169.40\n')             #These DNSs do not log your web-activity
-f.close()
-
-#Set up the monitor card
-interface = input("What wireless interface should be put into monitor mode? (e.g. wlan1)\n--> ")
-
+    if Refresh == 0:
+        print(""+ ask+"Starting DHCP address (ex: 10.0.0.50) ?\n")
+        DHCPStart = mod.VariableColoring("G",raw_input(answer))
+        Refresh = 1
+        continue
+    elif Refresh == 1:
+        print(""+ ask+"Stopping DHCP address (ex: 10.0.0.150) ?\n")
+        DHCPStop = mod.VariableColoring("G",raw_input(answer))
+        Refresh = 2
+        continue
+    while exit_the_loop == 0:
+        InfosHeader()
+        print(""+ask+"Do you confirm these settings ? Y/n\n")
+        confirm=raw_input(answer)
+        if confirm == "y" or confirm == "Y":
+            jump = 4
+            exit_the_loop = 1
+            break
+        elif confirm == "n" or confirm == "N":
+            DHCPStart = mod.VariableColoring("R","Not Configured")
+            DHCPStop = mod.VariableColoring("R","Not Configured")
+            Refresh = 0
+            break
+        else:
+            print("\n"+error + "Invalide Answer !")
+            slp(2)
+            continue
+    if exit_the_loop == 0:
+        continue
+        Refresh = 0
+    else:
+        Refresh = 0
+        break
+while jump == 4:
+    ################Create the dnsmasq config file
+    f = open('dnsmasq.conf', 'w')
+    f.write('interface=at0\n' +
+    'dhcp-range='+ DHCPStart + ',' + DHCPStop + ',12h\n' +
+    'server=80.67.169.12\n' +
+    'server=80.67.169.40\n')    #These DNSs do not log your web-activity
+    f.close()
+    system('dnsmasq -C dnsmasq.conf')
+    jump = 5
+    break
+while jump == 6:
+    ################Set up the monitor card
+    exit_the_loop = 0
+    InfosHeader()
+    if Refresh == 0:
+        print("" + ask +"Wireless interface to put into monitor mode ? (ex: wlan1)\n")
+        MONITOR = mod.VariableColoring("G", raw_input(answer))
+        Refresh = 1
+        continue
+    while exit_the_loop == 0:
+        InfosHeader()
+        print(""+ask+"Do you confirm this setting ? Y/n\n")
+        confirm=raw_input(answer)
+        if confirm == "y" or confirm == "Y":
+            jump = 6
+            exit_the_loop = 1
+            break
+        elif confirm == "n" or confirm == "N":
+            MONITOR = mod.VariableColoring("R", "Not Configured")
+            Refresh = 0
+            break
+        else:
+            print("\n"+error + "Invalide Answer !")
+            slp(2)
+            continue
+    if exit_the_loop == 0:
+        continue
+        Refresh = 0
+    else:
+        break
+while jump == 5:
+    ################Set the monitor card
+    InfosHeader()
+    exit_the_loop = 0
+    while exit_the_loop == 0:
+        InfosHeader()
+        print(errorsoft + "This operation will interrupt your Wifi connection")
+        print(errorsoft + "and you would have to restart Kali Linux to get")
+        print(errorsoft + "Debian to recognize your wireless card again.")
+        print(errorsoft + "However, the next step of this script will let")
+        print(errorsoft + "you connect to Wifi through Terminal.\n")
+        
+        print(""+ask+"Continue ? Y/n\n")
+        confirm=raw_input(answer)
+        if confirm == "y" or confirm == "Y":
+            jump = 7
+            exit_the_loop = 1
+            break
+        elif confirm == "n" or confirm == "N":
+            system('clear')
+            print("\n\n"+errorsoft+"Thank you for using EzRogueAP !")
+            slp(2)
+            system('clear')
+            exit()
+        else:
+            print("\n"+error + "Invalide Answer !")
+            slp(2)
+            continue
+    if exit_the_loop == 0:
+        continue
+    else:
+        break
 #Now set up airbase-ng and your fake AP
 
 ssid = input("What ESSID do you want to broadcast at? \n--> ")
 channel = input("On which channel do you want to emit ? 1-11")
-os.system('airmon-ng start ' + interface)
+system('airmon-ng start ' + interface)
 print "Now launching \"airbase-ng --essid %s mon0\"" % ssid
-os.system('airbase-ng --essid %s mon0 &>/dev/null &' % ssid)
-
-#Now Call DNSMASq to handle the DHCP for the connection
-os.system('dnsmasq -C dnsmasq.conf')
+system('airbase-ng --essid %s mon0 &>/dev/null &' % ssid)
 
 #Enable the at0 interface
 ip = starter.split('.')
 at_ip = ip[0] + '.' + ip[1] + '.' + ip[2] + '.1'
-os.system ('ifconfig at0 ' + at_ip + ' up netmask 255.255.255.0')
+system ('ifconfig at0 ' + at_ip + ' up netmask 255.255.255.0')
 
 #Now set up IPv4 Forwarding
 print "Making sure IPv4 forwarding is enabled..."
-os.system('echo \'1\' > /proc/sys/net/ipv4/ip_forward')
+system('echo \'1\' > /proc/sys/net/ipv4/ip_forward')
 
 #Now set up IPTables to be NAT
 print "almost done..."
 interwebz = raw_input("Enter the interface connected to the Interwebz (e.g. eth0)\n--> ")
 print "working some IPtables...I hate IPtables..."
-os.system('iptables --flush')
-os.system('iptables --table nat --flush')
-os.system('iptables --delete-chain')
-os.system('iptables --table nat --delete-chain')
-os.system('iptables --table nat --append POSTROUTING --out-interface %s -j MASQUERADE' % interwebz)
-os.system('iptables --append FORWARD --in-interface '+ interface +' -j ACCEPT')
+system('iptables --flush')
+system('iptables --table nat --flush')
+system('iptables --delete-chain')
+system('iptables --table nat --delete-chain')
+system('iptables --table nat --append POSTROUTING --out-interface %s -j MASQUERADE' % interwebz)
+system('iptables --append FORWARD --in-interface '+ interface +' -j ACCEPT')
 
 print "%s should be broadcasting, now go forth and profit" % ssid
